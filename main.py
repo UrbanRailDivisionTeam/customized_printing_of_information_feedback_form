7c46c9f63b5fc0182af2e67f015ce4844774d657
cheakf <sunmoming#gmail.com>
2026-06-03 16:12:29 +0800

    fix

--- main.py
+++ main.py
@@ -321,4 +321,4 @@ app = Litestar(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=12378)
+    uvicorn.run("main:app", host="0.0.0.0", port=12377)
